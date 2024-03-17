0aefd27763180c538bc2372d516e86fca5c5b5b3
lilpipidron <lociX55Lim@gmail.com>
2024-03-17 18:43:42 +0300

    test: add actor repository tests

---
+++ db/actor/actor_test.go
@@ -0,0 +1,137 @@
+package actor
+
+import (
+	actor2 "github.com/lilpipidron/vk-godeveloper-task/api/types/actor"
+	"github.com/lilpipidron/vk-godeveloper-task/api/types/gender"
+	mock_actor "github.com/lilpipidron/vk-godeveloper-task/mocks/db/actor"
+	"go.uber.org/mock/gomock"
+	"testing"
+	"time"
+)
+
+func TestActorRepository_addNewActor(t *testing.T) {
+	actor := actor2.Actor{
+		Name:        "name",
+		Surname:     "surname",
+		Gender:      gender.Male,
+		DateOfBirth: time.Now(),
+	}
+
+	c := gomock.NewController(t)
+	defer c.Finish()
+
+	db := mock_actor.NewMockActorRepository(c)
+
+	db.EXPECT().AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth).Return(nil)
+
+	err := db.AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func TestActorRepository_addAndDeleteActor(t *testing.T) {
+	actor := actor2.Actor{
+		Name:        "name",
+		Surname:     "surname",
+		Gender:      gender.Male,
+		DateOfBirth: time.Now(),
+	}
+
+	c := gomock.NewController(t)
+	defer c.Finish()
+
+	db := mock_actor.NewMockActorRepository(c)
+
+	db.EXPECT().AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth).Return(nil)
+
+	db.EXPECT().DeleteActor(gomock.Any()).Return(nil)
+
+	err := db.AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth)
+	if err != nil {
+		t.Errorf("Failed to add actor: %v", err)
+	}
+
+	err = db.DeleteActor(1)
+	if err != nil {
+		t.Errorf("Failed to delete actor: %v", err)
+	}
+}
+func TestActorRepository_addAndFindActor(t *testing.T) {
+	actor := actor2.Actor{
+		Name:        "name",
+		Surname:     "surname",
+		Gender:      gender.Male,
+		DateOfBirth: time.Now(),
+	}
+
+	c := gomock.NewController(t)
+	defer c.Finish()
+
+	db := mock_actor.NewMockActorRepository(c)
+
+	db.EXPECT().AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth).Return(nil)
+
+	db.EXPECT().FindActorsByNameAndSurname(actor.Name, actor.Surname).
+		Return([]*actor2.Actor{&actor}, nil)
+
+	err := db.AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth)
+	if err != nil {
+		t.Errorf("Failed to add actor: %v", err)
+	}
+
+	actors, err := db.FindActorsByNameAndSurname(actor.Name, actor.Surname)
+	if err != nil {
+		t.Errorf("Failed to find actor: %v", err)
+	}
+
+	if len(actors) != 1 {
+		t.Errorf("Expected to find 1 actor, found %d", len(actors))
+	}
+}
+
+func TestActorRepository_addActorChangeNameAndFindActor(t *testing.T) {
+	actor := actor2.Actor{
+		Name:        "name",
+		Surname:     "surname",
+		Gender:      gender.Male,
+		DateOfBirth: time.Now(),
+	}
+	newActor := actor2.Actor{
+		Name:        "new",
+		Surname:     "surname",
+		Gender:      gender.Male,
+		DateOfBirth: time.Now(),
+	}
+	newName := "new"
+	c := gomock.NewController(t)
+	defer c.Finish()
+
+	db := mock_actor.NewMockActorRepository(c)
+
+	db.EXPECT().AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth).Return(nil)
+
+	db.EXPECT().ChangeActorName(int64(1), newName).Return(nil)
+
+	db.EXPECT().FindActorsByNameAndSurname(newName, actor.Surname).
+		Return([]*actor2.Actor{&newActor}, nil)
+
+	err := db.AddNewActor(actor.Name, actor.Surname, actor.Gender, actor.DateOfBirth)
+	if err != nil {
+		t.Errorf("Failed to add actor: %v", err)
+	}
+
+	err = db.ChangeActorName(1, newName)
+	if err != nil {
+		t.Errorf("Failed to change actor's name: %v", err)
+	}
+
+	actors, err := db.FindActorsByNameAndSurname(newName, actor.Surname)
+	if err != nil {
+		t.Errorf("Failed to find actor: %v", err)
+	}
+
+	if len(actors) != 1 {
+		t.Errorf("Expected to find 1 actor, found %d", len(actors))
+	}
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/lilpipidron/vk-godeveloper-task
 go 1.22.1
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.8.4
+	go.uber.org/mock v0.4.0
 )
 
 require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/distribution/reference v0.5.0 // indirect
 	github.com/docker/docker v25.0.4+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
@@ -17,8 +21,10 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/tools v0.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
